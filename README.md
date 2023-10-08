0c17d6a73aef467b9cb4e14291b76eb3f551ff8c
zeio <zeionara@gmail.com>
2023-10-08 21:51:25 +0300

    init(repo): create the repo, implement sentiment classification using pipeline function

---
+++ README.md
@@ -0,0 +1,23 @@
+# mutant
+
+<p align='center'>
+    <img src='assets/logo.png'/>
+</p>
+
+Experiments with the latest releases of tensorflow and hugging face libraries
+
+## Running
+
+To run the currently configured experiment:
+
+```sh
+python -m mutant run 'Купил мужик шляпу, а она ему как раз'
+```
+
+## Installation
+
+To create `conda` environment and install dependencies:
+
+```sh
+conda env create -f environment.yml
+```
---
+++ mutant/__main__.py
@@ -0,0 +1,36 @@
+from os import environ as env
+
+env['TF_CPP_MIN_LOG_LEVEL'] = '3'  # disable verbose logging from tensorflow
+
+from click import group, argument
+from transformers import pipeline
+
+
+TOKEN = env['HUGGING_FACE_INFERENCE_API_TOKEN']
+
+
+@group()
+def main():
+    pass
+
+
+@main.command()
+@argument('text', type = str)
+def run(text: str):
+    # 1. Sentiment classification
+
+    # classifier = pipeline('sentiment-analysis', token = TOKEN)
+
+    # result = classifier(text)
+
+    # print(result)
+
+    # result = classifier([text, text])
+
+    # print(result)
+
+    print(text)
+
+
+if __name__ == '__main__':
+    main()
